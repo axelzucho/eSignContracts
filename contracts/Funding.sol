ab26c695f5b6f81d290bdf7e527fd84800dd9e5a
axelzucho <axelzucho1@gmail.com>
2020-03-19 19:50:00 -0600

    Implemented balances and donations (thanks to Salomon['Levy'] and Jonathan)  Person -> () | [] |

--- contracts/Funding.sol
+++ contracts/Funding.sol
@@ -1,9 +1,16 @@
 pragma solidity 0.5.16;
 
 contract Funding {
+  uint public raised;
   address public owner;
+  mapping(address => uint) public balances;
 
   constructor() public {
     owner = msg.sender;
+  }
+
+  function donate() public payable {
+    balances[msg.sender] += msg.value;
+    raised += msg.value;
   }
 }
