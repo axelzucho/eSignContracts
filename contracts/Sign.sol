14f6169f15c3279587fd677622253e512ce34355
Semy Levy <semylb@gmail.com>
2020-03-31 13:26:53 -0600

    Add interface for Sign contract. Add gitignore. Credits to Salomon, Jonathan and Axel

---
+++ contracts/Sign.sol
@@ -0,0 +1,33 @@
+pragma solidity 0.6.4;
+
+contract Sign {
+  address public owner;
+  
+  mapping(string => address[]) public signed; // Who has signed a given document
+  mapping(string => address[]) public signees; // Who is allowed to sign a given document
+  mapping(address => string[]) public owners; // What documents any given address owns
+
+  constructor() public {
+    owner = msg.sender;
+  }
+
+  // Creates a new entry for a hashed document, owned by sender
+  function uploadDocument(bytes memory document) public {
+
+  }
+
+  // Adds new signee address to document signee list
+  function addSignee(bytes memory document, address signee) public {
+
+  }
+
+  // Signs provided document if sender is allowed to
+  function sign(bytes memory document) public {
+
+  }
+
+  // Returns list of document signees
+  function getSignees(bytes memory document) public {
+
+  }
+}
