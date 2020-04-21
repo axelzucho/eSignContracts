44b498c138412f59b882a6159ea0a4c6fc7452e2
Jonathan Ginsburg <jginsburgn@gmail.com>
2020-04-21 18:09:56 -0500

    implement addSignee and getAllSignees

--- contracts/Sign.sol
+++ contracts/Sign.sol
@@ -1,49 +1,62 @@
 pragma solidity 0.6.4;
 
+
 contract Sign {
-  address public owner;
+    address public owner;
 
-  // A structure for uniquely identifying a document
-  struct Document {
-    address owner;
-    string document; // The word document is used instead of hash, because the latter is reserved
-  }
+    // Strcuture for indicating signees and status of signatures
+    struct Signature {
+        address signee;
+        bool isSigned;
+    }
 
-  // Strcuture for indicating signees and status of signatures
-  struct Signature {
-    address signee;
-    bool isSigned;
-  }
+    // Registry for documents and its signatures
+    mapping(bytes32 => Signature[]) public registry;
 
-  // Registry for documents and its signatures
-  mapping(bytes32 => Signature[]) public registry;
+    constructor() public {
+        owner = msg.sender;
+    }
 
-  constructor() public {
-    owner = msg.sender;
-  }
+    // Returns key, from a Document structure, to access Signatures in registry
+    function getKey(address documentOwner, string memory documentHash)
+        private
+        pure
+        returns (bytes32)
+    {
+        return keccak256(abi.encode(documentOwner, documentHash));
+    }
 
-  // Returns key, from a Document structure, to access Signatures in registry
-  function keyFromDocument(Document memory document) private pure returns (bytes32) {
-    return keccak256(abi.encode(document.owner, document.document));
-  }
+    // Adds new signee address to document signee list
+    function addSignee(string memory documentHash, address signee) public {
+        Sign.Signature[] storage signatures = registry[getKey(
+            msg.sender,
+            documentHash
+        )];
+        Sign.Signature memory signature = Signature(signee, false);
+        signatures.push(signature);
+    }
 
-  // Creates a new entry for a hashed document, owned by sender
-  function uploadDocument(bytes memory document) public {
+    // Signs provided document if sender is allowed to
+    function sign(address _owner, bytes memory document)
+        public
+        returns (bool)
+    {}
 
-  }
-
-  // Adds new signee address to document signee list
-  function addSignee(bytes memory document, address signee) public {
-
-  }
-
-  // Signs provided document if sender is allowed to
-  function sign(address _owner, bytes memory document) public returns(bool) {
-
-  }
-
-  // Returns list of document signees
-  function getSignees(address _owner, bytes memory document) public returns(address[] memory) {
-
-  }
+    // Returns list of document signees
+    function getAllSignees(address documentOwner, string memory documentHash)
+        public
+        view
+        returns (address[] memory)
+    {
+        Sign.Signature[] memory signatures = registry[getKey(
+            documentOwner,
+            documentHash
+        )];
+        address[] memory signatureAddresses = new address[](signatures.length);
+        for (uint8 index = 0; index < signatures.length; index++) {
+            Sign.Signature memory signature = signatures[index];
+            signatureAddresses[index] = signature.signee;
+        }
+        return signatureAddresses;
+    }
 }
